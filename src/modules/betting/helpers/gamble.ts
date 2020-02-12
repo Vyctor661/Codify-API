4d9876f4061e97659ffb201ae204a143dc761903
BlueGhostGH <engisoftleaderoff@gmail.com>
2020-02-12 23:11:53 +0200

    Added a gambling function helper

---
+++ src/modules/betting/helpers/gamble.ts
@@ -0,0 +1,10 @@
+import { randomInt } from "./randomInt"
+
+import { minReward, maxReward } from "../constants"
+
+export const gamble = (amount: number) => {
+    const dice = randomInt(minReward)(maxReward)
+    const winAmount = dice === 100 ? amount * 2 : dice >= 50 ? amount : -amount
+
+    return [dice, winAmount]
+}
