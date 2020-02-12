f0dd715c6702599ce3fbf387a402259a23cf9aa2
BlueGhostGH <engisoftleaderoff@gmail.com>
2020-02-12 21:28:10 +0200

    Added a class for HTTP Errors

---
+++ src/modules/error/classes/httpError.ts
@@ -0,0 +1,29 @@
+type ValueOf<T> = T[keyof T]
+
+const statusCodes = {
+    badRequest: 400,
+    notFound: 404,
+    internalServerError: 500
+} as const
+
+type HttpStatus = ValueOf<typeof statusCodes>
+
+const reasonToCodeMap: Record<HttpStatus, string> = {
+    "400": "Bad Request",
+    "404": "Not Found",
+    "500": "Internal Server Error"
+}
+
+export class HttpError extends Error {
+    public reason: string
+
+    constructor(
+        public status: HttpStatus,
+        customReason?: string,
+        public details?: object
+    ) {
+        super()
+
+        this.reason = customReason || reasonToCodeMap[status]
+    }
+}
