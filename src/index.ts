00d3576345965f5cb63cbee05e26c111eeb9b4a1
BlueGhostGH <engisoftleaderoff@gmail.com>
2020-02-12 21:45:19 +0200

    Rewritten main index file

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
-import koa from "koa"
+import Koa from "koa"
+
 import Router from "koa-router"
+
 import logger from "koa-logger"
+
+import { errorHandler } from "./modules/error/middleware/errorHandler"
 
 import mainRouter from "./routes/betRouter"
 
-import errorHandler from "./middleware/errorHandler"
-
-const app = new koa()
+const app = new Koa()
 const router = new Router()
 
-const port = process.env.PORT || 8080
+const port = Number(process.env.PORT ?? 8080)
+
+app.use(errorHandler())
 
 router.get("/", (ctx, next) => {
     ctx.body = "Hello."
@@ -17,11 +21,10 @@ router.get("/", (ctx, next) => {
 
 router.use("/user", mainRouter.routes())
 
-app.use(errorHandler)
 app.use(logger())
-app.use(router.routes())
-app.use(router.allowedMethods())
 
-app.listen(port, () => {
-    console.info(`Server started successfully at port ${port}`)
-})
+app.use(router.routes()).use(router.allowedMethods())
+
+const server = app.listen(port, () =>
+    console.info(`Koa app started and listening on the port ${port}! 🚀`)
+)
