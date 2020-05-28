b6b6d3b9467439608a3be90c1d66a77638fb7dae
Sandu Victor <vyctor661@gmail.com>
2020-05-28 22:00:39 +0300

    made gambling by api a little bit more innefficient

--- src/modules/betting/helpers/gamble.ts
+++ src/modules/betting/helpers/gamble.ts
@@ -4,7 +4,7 @@ import { minReward, maxReward } from "../constants"
 
 export const gamble = (amount: number) => {
     const dice = randomInt(minReward)(maxReward)
-    const winAmount = dice === 100 ? amount * 2 : dice >= 50 ? amount : -amount
+    const winAmount = dice === 100 ? amount * 2 : dice >= 50 ? amount : -amount*2
 
     return [dice, winAmount] as const
 }
